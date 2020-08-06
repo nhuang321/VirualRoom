cbd76112af704a349206e447da93bb6d5388a114
Jacob Ridgwell <jacobridgwell@gmail.com>
2020-08-06 16:50:07 -0700

    user login and db persistance works

--- static/login.js
+++
@@ -1,8 +0,0 @@
-function switchPage() {
-
-    if (document.getElementById("username").value && document.getElementById("webexlink").value) {
-        window.location.href='/game';
-    } else {
-        alert("Please enter the credentials first");
-    }
-}
