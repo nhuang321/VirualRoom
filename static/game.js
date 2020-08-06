9bc033161415a5bec2c6f8404ea44baa47c621ca
Jacob Ridgwell <jacobridgwell@gmail.com>
2020-08-06 15:19:18 -0700

    init

--- static/game.js
+++ static/game.js
@@ -49,7 +49,6 @@ canvas.width = 800;
 canvas.height = 600;
 var context = canvas.getContext('2d');
 socket.on('state', function(players) {
-  console.log(players);
   context.clearRect(0, 0, 800, 600);
   context.fillStyle = 'green';
   for (var id in players) {
